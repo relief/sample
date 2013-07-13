ed21f3fb73447c0f998b91b148ce4625cf97bb9a
relief <zjuxie@gmail.com>
2013-07-12 19:58:04 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,38 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+   # it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
   describe "Help pages" do
     it "should have the content 'Help'" do
-	visit '/static_pages/help'
+	visit help_path
 	page.should have_content('Help')
     end
   end
   describe "About page" do
     it "should have the content 'About Us'" do
-	visit '/static_pages/about'
+	visit about_path
 	page.should have_content('About Us')
+    end
+  end
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Our title | Contact")
     end
   end
 
